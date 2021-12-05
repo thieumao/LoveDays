f59e0943fd9f32ef7615fe4267fc7858a8e66603
Thieu Mao <thieumao@gmail.com>
2021-12-05 22:02:43 +0700

    Select photo

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,50 @@
-import * as React from 'react';
-import { Dimensions, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React, { FC, useState, useEffect } from 'react';
+import { Platform, Dimensions, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { Text, View } from '../components/Themed';
-
+import * as ImagePicker from 'expo-image-picker';
 import { RootStackScreenProps } from '../types';
 
 const screenWidth = Dimensions.get('window').width;
-
 const screenHeight = Dimensions.get('window').height;
 
+enum PERSON {
+  ONE = 'ONE',
+  TWO = 'TWO',
+}
+
 export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>) {
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  const [image1, setImage1] = useState(null);
+  const [image2, setImage2] = useState(null);
+  const pickImage = async (person: PERSON) => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled && result.uri) {
+      if (person === PERSON.ONE) {
+        setImage1(result.uri);
+      } else if (person === PERSON.TWO) {
+        setImage2(result.uri);
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.banner}>
@@ -18,11 +54,17 @@ export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>)
       <View style={styles.content}>
         <View style={styles.twoPeople}>
           <View style={styles.people}>
-            <Image style={styles.avatar} source={require("../assets/images/avatar.png")} />
+            <TouchableOpacity onPress={() => pickImage(PERSON.ONE)}>
+              {!image1 && <Image style={styles.avatar} source={require("../assets/images/avatar.png")} />}
+              {image1 && <Image style={styles.avatar} source={{ uri: image1 }} />}
+            </TouchableOpacity>
             <Text style={styles.name}>Person 1</Text>  
           </View>
           <View style={styles.people}>
-            <Image style={styles.avatar} source={require("../assets/images/avatar.png")} />
+            <TouchableOpacity onPress={() => pickImage(PERSON.TWO)}>
+              {!image2 && <Image style={styles.avatar} source={require("../assets/images/avatar.png")} />}
+              {image2 && <Image style={styles.avatar} source={{ uri: image2 }} />}
+            </TouchableOpacity>
             <Text style={styles.name}>Person 2</Text>  
           </View>
         </View>
